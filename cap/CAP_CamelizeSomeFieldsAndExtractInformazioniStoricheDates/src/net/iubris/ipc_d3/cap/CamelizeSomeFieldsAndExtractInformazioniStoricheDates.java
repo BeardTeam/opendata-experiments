a3fcdf17a15982939466ab3377afe018f04d0566
Massimiliano Leone <maximilianus@gmail.com>
2014-05-01 16:14:18 +0200

    added some raw data; fixed some data conversor (java); added cap counter

---
+++ cap/CAP_CamelizeSomeFieldsAndExtractInformazioniStoricheDates/src/net/iubris/ipc_d3/cap/CamelizeSomeFieldsAndExtractInformazioniStoricheDates.java
@@ -0,0 +1,146 @@
+package net.iubris.ipc_d3.cap;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.iubris.datautils.FileUtils;
+
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CamelizeSomeFieldsAndExtractInformazioniStoricheDates {
+
+	public static void main(String[] args) {
+		try {
+//			String input = args[0];
+			String input = "../../data/data_totale.csv";
+			String csvString = FileUtils.readFile(input, Charset.defaultCharset());
+			
+			csvString.replace("\"\"\"", "\"");
+			JSONArray adjustedTimeAndTipiSpecifici = new CamelizeSomeFieldsAndExtractInformazioniStoricheDates().adjustTimeAndTipiSpecifici(csvString);
+			String adjustedTimeAndTipiSpecificiString = adjustedTimeAndTipiSpecifici.toString();
+			System.out.println( adjustedTimeAndTipiSpecificiString );
+			FileUtils.writeToFile(adjustedTimeAndTipiSpecificiString, "../data_totale_camelized_some_fields.json");
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private JSONArray adjustTimeAndTipiSpecifici(String dataAsCSV) throws ParseException {		
+		JSONArray jsonArray = CDL.toJSONArray(dataAsCSV);
+		int length = jsonArray.length();
+		JSONArray jsonArrayNew = new JSONArray();
+		for (int i=0;i<length;i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+			
+			JSONObject jsonObjectNew = new JSONObject();
+			
+			jsonObjectNew.put("nome",jsonObject.getString("nome"));
+			jsonObjectNew.put("indirizzo",jsonObject.getString("indirizzo"));
+			jsonObjectNew.put("numeroCivico",jsonObject.getString("numero-civico"));
+			jsonObjectNew.put("cap",jsonObject.getString("cap"));
+			jsonObjectNew.put("quartiere",jsonObject.getString("quartiere"));
+			jsonObjectNew.put("citta",jsonObject.getString("citta"));
+			jsonObjectNew.put("geolocazione",jsonObject.getString("geolocazione"));
+			jsonObjectNew.put("telefono",jsonObject.getString("telefono"));
+			jsonObjectNew.put("mobile",jsonObject.getString("mobile"));
+			jsonObjectNew.put("email",jsonObject.getString("email"));
+			jsonObjectNew.put("web",jsonObject.getString("web"));
+			jsonObjectNew.put("tipi",jsonObject.getString("tipi"));
+			jsonObjectNew.put("tipiSpecifici",jsonObject.getString("tipi-specifici"));
+			
+//			jsonObjectNew.put("tipiSpecificiReduced", getTipiReduced(jsonObject));
+//			jsonObjectNew.put("times", getTimes(jsonObject));
+			
+			LinkedList<String> date = findNumbers( jsonObject.getString("luogo-da-visitare.informazioni_storiche"));
+			String dateString = date.toString().replace("[", "").replace("]", "");
+			jsonObjectNew.put("luoghiDaVisitare.informazioniStoriche.date", dateString);
+			
+			jsonArrayNew.put(jsonObjectNew);
+		}
+		return jsonArrayNew;
+	}
+	
+	private LinkedList<String> findNumbers(CharSequence line) {
+		LinkedList<String> numbers = new LinkedList<String>();
+
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher(line); 
+		while (m.find()) {
+		   numbers.add(m.group());
+		}
+		return numbers;
+	}
+	
+	private String getTipiReduced(JSONObject jsonObject) {
+		String tipiSpecifici = jsonObject.getString("tipi-specifici");
+		String tipiSpecificiReduced = "";
+		switch (tipiSpecifici) {
+			case "Discoteca":
+				tipiSpecificiReduced = "Discoteca";
+				break;
+			case "Pub, DiscoPub":
+				tipiSpecificiReduced = "DiscoPub";
+				break;
+			case "Cocktail Bar":
+				tipiSpecificiReduced = "Cocktail";
+				break;
+			case "Cocktail Bar, American Bar, Ristorante":
+				tipiSpecificiReduced = "American";
+				break;
+			case "Cocktail Bar, Pizzeria, Ristorante, Panineria":
+				tipiSpecificiReduced = "Panineria";
+				break;
+			case "Wine Bar, Pub":
+				tipiSpecificiReduced = "Pub";
+				break;
+			case "Wine Bar":
+				tipiSpecificiReduced = "Wine";
+				break;
+			default:
+				tipiSpecificiReduced = "";
+				break;
+		}
+		return tipiSpecificiReduced;
+	}
+	
+	private JSONArray getTimes(JSONObject jsonObject) throws ParseException{
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		String date = "1970-01-01";
+		JSONArray times = new JSONArray();
+		String apertura = jsonObject.getString("apertura");
+		long startingTimeEpoch = 0;
+		if (apertura.isEmpty())
+			apertura = "19.00"; // default
+		startingTimeEpoch = sdf.parse(date+" "+apertura.replace(".", ":")).getTime();
+		
+		String chiusura = jsonObject.getString("chiusura");
+		long endingTimeEpoch = 0;
+		if (chiusura.isEmpty())
+			chiusura = "1.00"; // default
+		double parsedEndingTime = Double.parseDouble(chiusura);
+		if ( parsedEndingTime >= 0f && parsedEndingTime < 6f ) // we are in next day
+			date = "1970-01-02";
+		endingTimeEpoch = sdf.parse(date+" "+chiusura.replace(".", ":")).getTime();
+		
+		String startingTimeEpoched = ""+startingTimeEpoch;
+		String endingTimeEpoched = ""+endingTimeEpoch;
+		JSONObject et = new JSONObject();
+		et.put("starting_time", startingTimeEpoched);
+		et.put("ending_time", endingTimeEpoched);
+		times.put(et);
+		
+		return times;
+	}
+}
---
+++ cap/json_query.js
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+
+//var nameFileWithPathWithoutExtension = "data_totale_camelized_some_fields";
+var nameFileWithPathWithoutExtension = "data_totale_camelized_some_fields";
+//var csvFile =  nameFileWithPathWithoutExtension+".csv";
+var jsonFile = nameFileWithPathWithoutExtension+".json";
+
+// loading json zone
+var json;
+$.ajax({
+	url: jsonFile,
+	async: true,
+	dataType: 'text/json',
+	timeout: 10000,
+	success: parse,
+	error: handleError
+});
+function parse(data) {
+	json = JSON.parse(data);
+	var queryMap = [
+	  /*{key:"tipiSpecifici",value:"Discoteca"}
+	  ,{key:"tipiSpecifici",value:"Pub, Discopub"},
+	  {key:"tipi",value:"accoglienza"},
+	  {key:"tipiSpecifici",value:"Hotel"},
+	  {key:"tipiSpecifici",value:"Bed and Breakfast"},
+	  {key:"tipiSpecifici",value:"Chiesa"},
+	  {key:"tipiSpecifici",value:"Palazzo"},
+	  {key:"tipiSpecifici",value:"Galleria artistica, Museo"}
+	  ,*/{key:"tipi",value:"ristoro"}
+	  ,{key:"tipi",value:"divertimento, ristoro"}
+    ];
+	for (var q=0;q<queryMap.length;q++) {
+		doQuery(json,queryMap[q].key,queryMap[q].value);
+	}
+
+//	doQuery(json);
+}
+function handleError(data) {
+	if (data.readyState === 4 && data.status === 200 && data.statusText === "OK")
+		parse(data.responseText);
+	else
+		console.error(data);
+}
+
+function doQuery(json, key, value) {
+//	var query = $objeq("nome =~ %1 -> {nome: nome, geolocazione: geolocazione, indirizzo: indirizzo, numeroCivico: numeroCivico, telefono: telefono, mobile: mobile, web: web, tipiSpecifici: tipiSpecifici}",place.name);
+//	var query = $objeq("tipiSpecifici =~ %1 -> {cap: cap} ","DiscoPub");
+/*	var query = $objeq(key+" =~ %1 -> {cap: cap}",value);	
+	var res = query(json);
+	console.log(res);
+	var capsRepeated = [];
+	for (var i=0;i<res.length;i++) {
+		var cap = res[i].cap;
+		capsRepeated.push(cap);
+	}
+	var caps = capsRepeated.filter( onlyUnique );
+	*/
+	var capsCountMap = getCapsCountMap(json,key,value);
+//	console.log(caps);
+	/*var capsCounted = count(caps);
+	console.log(capsCounted);
+	var capsN = capsCounted[0];
+	var counted = capsCounted[1];
+	*/
+	var htmlResult = "<div><b>"+value+"</b></div>";
+	for (var c in capsCountMap) {
+		htmlResult+= "<div>"+c+" "+capsCountMap[c]+"</div>";
+	}
+//	var htmlResult = "<div><b>"+value+"</b></div>";
+//	capsCountMap
+//	htmlResult += "<div>--</div>";
+	var currentHtml = $('#out').html();
+	$('#out').html(currentHtml+htmlResult);
+}
+
+
+function count(arr) {
+    var a = [], b = [], prev;
+
+    arr.sort();
+    for ( var i = 0; i < arr.length; i++ ) {
+        if ( arr[i] !== prev ) {
+            a.push(arr[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = arr[i];
+    }
+    return [a, b];
+}
+
+function getCapsCountMap(json,key,value) {
+	var queryString = key+" IN %1 -> {nome: nome, tipiSpecifici: tipiSpecifici, cap: cap}";
+//	console.log(queryString);
+	var query = $objeq(queryString,value);
+//	var query = $objeq("{cap: cap}");
+//	console.log(query);
+	var res = query(json);
+//	console.log(res);
+	//var capsRepeated = [];
+	var capsCountMap = new Object();
+	for (var i=0;i<res.length;i++) {
+		var cap = res[i].cap;		
+//		console.log("testing "+cap);
+		if (!capsCountMap.hasOwnProperty(cap))
+			capsCountMap[cap] = 1;
+		else
+			capsCountMap[cap] = capsCountMap[cap] +1;
+//		console.log(caps);
+//		capsRepeated.push(cap);
+	}
+//	console.log(Object.keys(caps));
+//	console.log(capsCountMap);
+	//var caps = capsRepeated.filter( onlyUnique );
+	
+	return capsCountMap;
+//	var caps = {}
+}
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
